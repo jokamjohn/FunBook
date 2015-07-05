427898dc911096539df5356f002a728daed84409
jokamjohn <johnkagga@gmail.com>
2015-07-05 20:57:27 +0300

    UI build up

--- app/src/main/java/me/johnkagga/funbook/UI/BodyActivityFragment.java
+++ app/src/main/java/me/johnkagga/funbook/UI/BodyActivityFragment.java
@@ -19,6 +19,7 @@ public class BodyActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_body, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_body, container, false);
+        return rootView;
     }
 }
